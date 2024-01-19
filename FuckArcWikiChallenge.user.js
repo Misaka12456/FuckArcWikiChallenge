b9cc7f0d2f44857bb15004de84d809df5fdcb306
御坂12456 <a15951@qq.com>
2024-01-20 00:52:27 +0800

    Update to 0.3.2
    - Adapted to new domain server of Arcaea Wiki (cn)
    - Fixed a bug which cause failing to skip authentication in some cases

--- FuckArcWikiChallenge.user.js
+++ FuckArcWikiChallenge.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         FuckArcWikiChallenge
-// @namespace    https://github.com/Misaka12456/FuckArcWikiChallenge
-// @version      0.3.0
+// @namespace    https://misakacastle.moe
+// @license      WTFPL
+// @version      0.3.2
 // @description  绕过Arcaea中文Wiki的技术类隐藏页面验证
 // @author       Misaka12456
-// @match        https://wiki.arcaea.cn/*
+// @match        https://arcwiki.mcd.blue/*
 // @icon         https://pic.rmb.bdstatic.com/bjh/3a01f3977ca05bb86115fe2fb144ba21.png
 // @grant        none
 // @require      https://unpkg.com/jquery@3.6.0/dist/jquery.min.js
@@ -13,8 +14,6 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 console.log('FuckArcWikiChallenge Loaded');
 $(document).ready(function () {
 	if ($('#autohidden').length > 0) {
-		document.evaluate('/html/body/div/div/div[1]/div[3]/main/div[2]/div[3]/div[1]/table/tbody/tr/td[1]/div/img', document).iterateNext().src = "https://pic.rmb.bdstatic.com/bjh/3a01f3977ca05bb86115fe2fb144ba21.png";
-		console.debug('[FuckArcWikiChallenge]Replaced Visibility Icon');
 		$('#autohidden').replaceAll('<p></p>');
 		console.debug('[FuckArcWikiChallenge]Cleared Question Div');
 		$('#autoshow').removeAttr('class', 'test-hidden-2');
